72fb84b9fc6730ed316c83421f882be17ec030e3
Jenloke <mjenloke@gmail.com>
2022-11-30 19:44:07 +0800

    remove Main.java comments

--- src/Main.java
+++ src/Main.java
@@ -2,48 +2,5 @@ public class Main {
     public static void main(String[] args) {
         UI init = new UI();
         init.init();
-
-
-        /*
-        possible var in driver code
-            betSize should be taken here
-        */
-
-        //should only be possible when creating user
-        //this object should be newUser instead of user1
-        // default constructor should be
-
-//        System.out.println("Are you a new user?");
-//        String newUserAnswer = read.nextLine();
-//
-//        if (newUserAnswer == "Yes") {
-//            String inputUserName;
-//            System.out.println("UserName:");
-//            inputUserName = read.nextLine();
-//
-//            //users.BasicUser user1 = new users.BasicUser();
-//            System.out.println("Want to play now?");
-//        }
-//        the user that is currently playing should be currentUser not user1
-//        users.BasicUser user1 = new users.BasicUser();
-//
-//        user1.display();
-//
-
-//
-//        System.out.println(user1.getWallet());
-
-        /*
-            Game Logic
-               enter bet amount
-               select color
-
-               store in a varColor  game.getRandomColor()
-
-               evaluate input against varColor
-                 if win wallet
-
-               need a transaction class
-        */
     }
 }
