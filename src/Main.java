de94497adcec546010d096a0577ce6b38d2f1770
Jenloke <mjenloke@gmail.com>
2022-12-05 16:21:14 +0800

    add name & section comment at main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
+/*
+  Jenloke N. Magbojos
+  CS-2101
+  CS 211 - Final Requirement
+  Option #2
+ */
+
 public class Main {
     public static void main(String[] args) {
         UI init = new UI();
